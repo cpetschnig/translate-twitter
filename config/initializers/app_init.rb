5201b3415ba65946d82ec4b1ee2ba6139dbe1c92
Christoph Petschnig <info@purevirtual.de>
2010-05-04 22:00:37 +0200

    Added Google Analytics support

--- config/initializers/app_init.rb
+++ config/initializers/app_init.rb
@@ -1,3 +1,13 @@
 require File.join Rails.root, 'app/models', 'twitter_status'
 require File.join Rails.root, 'app/models', 'twitter_user'
 require File.join Rails.root, 'app/models', 'constantrecords'
+
+def get_google_analytics_id
+  filename = File.join(Rails.root, 'config', 'google-analytics.yml')
+  yml_result = YAML::load(File.read(filename))
+  yml_result['web_property_id']
+end
+
+
+GOOGLE_ANALYTICS_ID = get_google_analytics_id
+
