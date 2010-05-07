1d66e56dc8f77d0c95dc677b4c088d60e3a5542c
Christoph Petschnig <info@purevirtual.de>
2010-05-07 14:57:25 +0200

    More layout improvements

--- app/helpers/showcase_helper.rb
+++ app/helpers/showcase_helper.rb
@@ -9,7 +9,7 @@ module ShowcaseHelper
   end
 
   private
-  def user_account_list_item(name_1, name_2, screen_name, img_url, img_alt, active)
+  def user_account_list_item_(name_1, name_2, screen_name, img_url, img_alt, active)
     content = %{<img src="#{img_url}" alt="#{img_alt}" class="tw_normal" />
 <span class="screen_name">#{name_1}</span><br />
 #{name_2}}
@@ -24,5 +24,17 @@ module ShowcaseHelper
 </a></li>
 }
     end.html_safe
+  end
+
+  def user_account_list_item(name_1, name_2, screen_name, img_url, img_alt, active)
+    %{<li#{' class="active"' if active}>
+      <a href="/#{screen_name}" class="name">
+        <span class="screen_name">#{name_1}</span><br />
+        #{name_2}
+      </a>
+      <a href="/#{screen_name}" class="thumb">
+        <img src="#{img_url}" alt="#{img_alt}" title="#{screen_name}" class="tw_normal" />
+      </a>
+    </li>}.html_safe
   end
 end
