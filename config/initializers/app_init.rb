0d1763c263ac6b89705e61479b9983d08ef6b8ce
Christoph Petschnig <info@purevirtual.de>
2010-05-19 10:49:49 +0200

    Made Google Analytics optional

--- config/initializers/app_init.rb
+++ config/initializers/app_init.rb
@@ -4,6 +4,12 @@ require File.join Rails.root, 'app/models', 'constantrecords'
 
 def get_google_analytics_id
   filename = File.join(Rails.root, 'config', 'google-analytics.yml')
+
+  unless File.exist?(filename)
+    puts "#{filename} not found. Google Analytics will not be avaliable!"
+    return nil
+  end
+
   yml_result = YAML::load(File.read(filename))
   yml_result['web_property_id']
 end
