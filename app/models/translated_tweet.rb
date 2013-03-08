76611533bcc862690d16a3710f92f00e206508e5
Christoph Petschnig <info@purevirtual.de>
2013-03-08 22:15:03 +0100

    Repair bad translations

--- app/models/translated_tweet.rb
+++ app/models/translated_tweet.rb
@@ -3,7 +3,21 @@ class TranslatedTweet < ActiveRecord::Base
 
   belongs_to :tweet
 
+  before_save :repair_translation
+
   validates :text, :length => {:maximum => 512}
 
   attr_accessible :text, :service_id
+
+  private
+
+  def repair_translation
+    # check for twitter account translations: '@username' might have become '@ username'
+    self.tweet.text.scan(/@[a-zA-Z0-9_]+/) do |match|
+      # the translator likes to break twitter usernames
+      # "@nari3" became "@ nari 3" or even "@ Nari 3"
+      broken_username = match[1..-1].gsub(/(\d+)/, ' \1 ').strip
+      self.text.gsub!(%r{@ (#{broken_username}|#{broken_username.capitalize})}, match)
+    end
+  end
 end
--- spec/models/translated_tweet_spec.rb
+++ spec/models/translated_tweet_spec.rb
@@ -14,4 +14,16 @@ describe TranslatedTweet do
     it { should allow_mass_assignment_of :text }
     it { should allow_mass_assignment_of :service_id }
   end
+
+  describe "#repair_translation" do
+    before do
+      subject.tweet = Tweet.new(:text => "@nzkoz @steveklabnik @indirect Ya, I said we could CC there.")
+      subject.text = "@nzkoz @ steveklabnik @ indirect Ya, I said we could CC there."
+    end
+
+    it "should remove spaces in usernames" do
+      subject.save
+      subject.text.should == "@nzkoz @steveklabnik @indirect Ya, I said we could CC there."
+    end
+  end
 end
