1951660fd1a72a162f78527905b78ed35de20d64
Irysomka <iryna.makar@gmail.com>
2015-10-29 19:22:24 +0100

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'translate_twitter'
+set :repo_url, 'git@github.com:Toptranslation/translate-twitter.git'
 
+set :app_user, 'translate_twitter'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/srv/translate_twitter/'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -20,13 +21,15 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, %w{config/database.yml config/environments/production.rb .ruby-version}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w{files log pids tmp/cache public/assets public/uploads db/backup .git tmp/pids}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,7 +37,27 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :rbenv_type, :system
+set :rbenv_ruby, '2.2.2'
+
+# capistrano3-unicorn
+set :unicorn_pid, '/srv/translate_twitter/shared/pids/unicorn.pid'
+
 namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+    # invoke 'logs:production_cycle'
+    invoke 'unicorn:reload'
+    invoke 'delayed_job:restart'
+    invoke 'notify_rollbar'
+  end
+
+  after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
