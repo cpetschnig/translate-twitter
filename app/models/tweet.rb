16fd548540117099d2aea2192f3fa594ae07fa56
Stefan Rohde <stefan.rohde@toptranslation.com>
2015-11-06 19:06:54 +0100

    use to_s for strings in tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -18,9 +18,9 @@ class Tweet < ActiveRecord::Base
   def self.from_twitter(obj)
     self.new(:text => obj.text,
              :twitter_id => obj.id,
-             :irt_screen_name => obj.in_reply_to_screen_name,
+             :irt_screen_name => obj.in_reply_to_screen_name.to_s,
              :irt_user_id => obj.in_reply_to_user_id,
-             :irt_status_id => obj.in_reply_to_status_id,
+             :irt_status_id => obj.in_reply_to_status_id.to_s,
              :source => obj.source,
              :tw_created_at => obj.created_at)
   end
