b0766d3858fabc0286791366fa5fd7d346af5f8e
Coma2nd <stefan.rohde@toptranslation.com>
2015-11-18 18:49:21 +0100

    remove location and description

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -43,8 +43,6 @@ class TwitterAccount < ActiveRecord::Base
     result = TwitterClient.global.user(self.username)
 
     self.user_id = result.id
-    self.location = result.location
-    self.description = result.description
     self.created_at_twitter = result.created_at
     self.real_name = result.name
     self.followers = result.followers_count
